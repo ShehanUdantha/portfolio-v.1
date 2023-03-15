895b7e5bfa6eabebbd708bb11e999be3c44b33f5
Wickramasinghe Mudiyanselaghe Shehan Udantha Baddekumbura <shehansub@gmail.com>
2023-03-15 10:32:20 +0530

    fixed warning

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -173,18 +173,6 @@ const Projects = () => {
                   <p>Code</p>
                   <FaGithub className="h-[21px] w-[21px]" />
                 </a>
-                <a
-                  className="flex gap-1 items-center cursor-pointer hover:opacity-50 transition duration-500 hidden justify-center md:justify-start md:w-auto"
-                  href="https://github.com/ShehanUdantha"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <p>Online</p>
-                  <span className="relative flex h-3 w-3">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-                  </span>
-                </a>
               </motion.div>
             </div>
           </div>
